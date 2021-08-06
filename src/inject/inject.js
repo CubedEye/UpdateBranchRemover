4526909707339256736c5634e1510173adfdcb8c
Mohammad Takbiri <mohammad@klue.com>
2021-08-06 00:46:15 -0700

    refactor: small adjustments

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,11 +3,6 @@
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
 		const element = Array.from(document.querySelectorAll('button')).find(el => el.textContent.trim() === 'Update branch');
 		if(element) {
 			element.parentElement.removeChild(element);
@@ -15,4 +10,4 @@
 			
 
 	}
-	}, 2000);
+	}, 700);
